b1746d4ab3feea487136823999b38e7df21078fc
vinay <58105215+vinay-ardhani01010@users.noreply.github.com>
2020-08-10 18:40:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>PHP URL Shortener Without Database!</title>
-<meta name="description" content="PHP URL Shortener Without Database!">
+<meta name="description" content="">
 </head>
 <body>
 
@@ -21,9 +20,6 @@
 		<p>Enter a long link: <input type="text" name="my-link" value=""></p>
 		<p><input type="submit" value="Shorten!"></p>
 	</form>
-
-	<h2>Download & Usage</h2>
-	<p><a href="http://wp-time.com/create-url-shortener-without-database/" target="_blank">PHP URL Shortener Without Database</a></p>
 
 </body>
 </html>
